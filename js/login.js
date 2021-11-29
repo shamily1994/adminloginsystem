4bd3ef289f1f1f8176378d595e7c5a2ed7f930ea
shamily1994 <shamilysasidharan111@gmail.com>
2021-11-29 20:00:43 +0530

    removed unwanted code and changed the background properties

--- js/login.js
+++ js/login.js
@@ -6,7 +6,8 @@ function validate() {
 
 
     if (username == "admin" && password == "12345") {
-        alert("logged in");
+        // alert("logged in");
+        window.location.href(home.html);
         
         // return true;
         // window.location.replace("index.html");
@@ -15,33 +16,33 @@ function validate() {
 
     }
     else {
-        alert("login failed");
+        // alert("login failed");
         event.preventDefault();
     }
 }
 
-// const submit = document.getElementById("submit");
+const submit = document.getElementById("submit");
 
-// submit.addEventListener('click', validate);
+submit.addEventListener('click', validate);
 
-// function validate(e) {
-//   e.preventDefault();
+function validate(e) {
+  e.preventDefault();
 
-//   const userNameField = document.getElementById("username");
-//   let valid = true;
+  const userNameField = document.getElementById("username");
+  let valid = true;
 
-//   if (!userNameField.value) {
-//     const nameError = document.getElementById("nameError");
-//     nameError.classList.add("visible");
-//     userNameField.classList.add("invalid");
-//     nameError.setAttribute('aria-hidden', false);
-//     nameError.setAttribute('aria-invalid', true);
+  if (!userNameField.value) {
+    const nameError = document.getElementById("nameError");
+    nameError.classList.add("visible");
+    userNameField.classList.add("invalid");
+    nameError.setAttribute('aria-hidden', false);
+    nameError.setAttribute('aria-invalid', true);
 
-//   }
+  }
 
 
-//   return valid;
-// }
+  return valid;
+}
 
 
 
